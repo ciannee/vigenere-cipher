d5694b55ff53556cf2ed6e155244b8a6628735d7
Patricia Pineda <pepineda28@gmail.com>
2023-04-16 01:29:31 +0800

    pseudocode

---
+++ vigenere-cipher.py
@@ -0,0 +1,27 @@
+# Name: Pineda, Patricia Anne E.
+# Section: BSCPE 1-5
+# Assignment 3 : Redo Problem 3 with multiple github commits (Vigenere Cipher)
+
+# looping
+
+# build class of colors to print color text
+
+    #ask for user's input
+
+    #import pyfiglet
+
+    #print output
+    
+    # convert letter to index and index to letter
+
+        # split the message to the length of the key
+       
+        # convert message to index and add key
+
+        # split ciphertext to the length of key
+       
+        # convert ciphertext to index and subtract key 
+
+    # looping
+
+        # print outro
