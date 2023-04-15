35f8f9780e325fc5f85abd706e10b10734d17885
Patricia Pineda <pepineda28@gmail.com>
2023-04-16 01:43:30 +0800

    convert ciphertext to index and subtract key

--- vigenere-cipher.py
+++ vigenere-cipher.py
@@ -78,9 +78,22 @@ while cipher_str != "no":
 
         # split ciphertext to the length of key
         split_cipher = [cipher[i:i + len(key)] for i in range(0, len(cipher), len(key))] # start, end, step
-        
-        # convert ciphertext to index and subtract key 
 
+        # convert ciphertext to index and subtract key 
+        for each_split in split_cipher:
+            i = 0
+            for letter in each_split:
+                number = (letter_to_index[letter] - letter_to_index[key[i]]) % len(alphabet)
+                decrypted += index_to_letter[number]
+                i += 1
+        
+        return decrypted
+
+    def main ():
+        key = "knights"
+        message = "thisisthelasttaskhoorday"
+        encrypted_message = encrypt(message, key)
+        
     # looping
 
         # print outro
